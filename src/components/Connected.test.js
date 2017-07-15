ae35e9e69abf3688c5794c2227109ac16ac678a5
Gavin Daly <gavin@integro.co>
2017-07-15 13:47:52 -0400

    more

--- src/components/Connected.test.js
+++ src/components/Connected.test.js
@@ -11,14 +11,12 @@ describe('<Connected>', () => {
     wrapper = shallow(<Connected connected='online' disconnected='offline'/>)
   })
 
-
   it('should show the connected prop when online', () => {
     online.mockReturnValue(true)
     expect(
       wrapper.contains('online')
     ).toBe(true)
   })
-
 
   it('should show the disconnected prop when offline', () => {
     online.mockReturnValue(false)
