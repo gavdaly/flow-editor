3357f88eb66e6931a62f2d6f8026a3628d11c30c
Gavin Daly <gavin@integro.co>
2017-07-16 11:43:03 -0400

    add component wrapper

--- src/components/FlowEditor.jsx
+++ src/components/FlowEditor.jsx
@@ -10,44 +10,50 @@ import {
   // convertToRaw
 } from 'draft-js'
 
+import { observer } from 'mobx-react'
+import { observable } from 'mobx'
 
+const state = observable({
+  test: 'one'
+})
 
 class FlowEditor extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      editorState: EditorState.createEmpty()
-    }
-    this.onChange = (editorState) => this.setState({editorState})
+  state = {
+    editorState: EditorState.createEmpty()
   }
   _click = () => {
+    console.log(state)
     this.refs.editor.focus()
   }
 
+  onChange = (editorState) => this.setState({editorState})
 
   render() {
     return (
-      <div onClick={this._click} className='editorWrapper'>
-        {{
-          write: <Editor
-            editorState={this.state.editorState}
-            onChange={this.onChange}
-            placeholder='Write Ready to get in the flow...'
-            ref='editor'
-          />,
-          edit: <Editor
-            editorState={this.state.editorState}
-            onChange={this.onChange}
-            placeholder='Nothing to edit'
-            ref='editor'
-          />,
-          format: <Editor
-            editorState={this.state.editorState}
-            onChange={this.onChange}
-            placeholder='Nothing to format'
-            ref='editor'
-          />
-        }[this.props.mode]}
+      <div className='documentWrapper'>
+        <input placeholder='title' />
+        <div onClick={this._click} className='editorWrapper'>
+          {{
+            write: <Editor
+              editorState={this.state.editorState}
+              onChange={this.onChange}
+              placeholder='Write Ready to get in the flow...'
+              ref='editor'
+            />,
+            edit: <Editor
+              editorState={this.state.editorState}
+              onChange={this.onChange}
+              placeholder='Nothing to edit'
+              ref='editor'
+            />,
+            format: <Editor
+              editorState={this.state.editorState}
+              onChange={this.onChange}
+              placeholder='Nothing to format'
+              ref='editor'
+            />
+          }[this.props.mode]}
+        </div>
       </div>
     )
   }
