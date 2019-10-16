1e925d32ebcaecb246260201921d82a1f9d810bc
Gavin Daly <gavin@gav.local>
2019-10-16 11:21:51 -0400

    in memory state for context

--- src/hooks/useDocuments.tsx
+++ src/hooks/useDocuments.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import React, { useState, useContext, createContext } from "react";
 
 export interface IDocument {
   title: string;
@@ -12,3 +12,39 @@ export enum Mode {
   Edit = "edit",
   Format = "format"
 }
+
+interface IDocumentContext {
+  documents: IDocument[];
+  updateDocument: (document: IDocument) => void;
+  deleteDocument: (document: IDocument) => void;
+}
+
+const DocumentContext = createContext<Partial<IDocumentContext>>({});
+
+const DocumentProvider: React.FC = props => {
+  const [documents, setDocuments] = useState<IDocument[]>([]);
+
+  function updateDocument(document: IDocument) {
+    setDocuments(
+      documents.map(doc => {
+        if (doc.id === document.id) return document;
+        return doc;
+      })
+    );
+  }
+
+  function deleteDocument(document: IDocument) {
+    setDocuments(documents.filter(doc => doc.id !== document.id));
+  }
+
+  return (
+    <DocumentContext.Provider
+      value={{ documents, updateDocument, deleteDocument }}
+      {...props}
+    />
+  );
+};
+
+const useDocument = () => useContext(DocumentContext);
+
+export { DocumentProvider, useDocument };
